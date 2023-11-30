74b2681a7cfa77367ea00160abdb0bb7b77eda4c
okamoto <j2008@matsue-ct.ac.jp>
2023-11-30 16:02:21 +0900

    kadai2 commit

--- kadai2-2.c
+++ kadai2-2.c
@@ -6,6 +6,14 @@ int main
 	for(int i = 1;i<=100;i++)
 	{
 		a+=i;
+		if(a%3==0)
+		{
+			ptintf("fizz\n");
+		}
+		else if(a%5==0)
+			printf("buzz\n");
+		else if(a%3==0&&a%5==0)
+			printf("fizzbuzz\n");
 	}
 	return 0;
 }
